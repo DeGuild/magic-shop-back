c99ed4fd3a1b11df0c40a3d32dc4ecd8cdb8db13
parmcoder <possawat2017@gmail.com>
2021-10-06 21:51:35 +0700

    working on CRUDs

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # magic-shop-back
 Mostly, handle the microservices for magic shop
+
+Use this to develop your code locally
+
+    firebase emulators:start 
+
+Use this to deploy
+
+    firebase deploy
--- functions/index.js
+++ functions/index.js
@@ -56,3 +56,63 @@ exports.readMagicScroll = functions.https.onRequest(async (req, res) => {
     });
   }
 });
+
+exports.deleteCertificate = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const address = req.body.address;
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  await admin.firestore().collection("certificate/").doc(`${address}`).delete();
+
+  // Send back a message that we've successfully written the message
+  res.json({
+    result: "Successful",
+  });
+});
+
+exports.allCertificates = functions.https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const lastItem = req.path;
+  let data = [];
+  if (lastItem.length > 1) {
+    const paths = lastItem.split("/");
+    if (paths[2] === "next") {
+      const startAtSnapshot = admin
+        .firestore()
+        .collection("certificate/")
+        .orderBy("address", "desc")
+        .startAfter(paths[1]);
+
+      const items = await startAtSnapshot.limit(8).get();
+      items.forEach((doc) => {
+        data.push(doc.id);
+      });
+    } else if (paths[2] === "previous") {
+      const startAtSnapshot = admin
+        .firestore()
+        .collection("certificate/")
+        .orderBy("address", "asc")
+        .startAfter(paths[1]);
+
+      const items = await startAtSnapshot.limit(8).get();
+      items.forEach((doc) => {
+        data.push(doc.id);
+      });
+    }
+  } else {
+    const readResult = await admin
+      .firestore()
+      .collection("certificate/")
+      .orderBy("address", "desc")
+      .limit(8)
+      .get();
+    // Send back a message that we've successfully written the message3
+    readResult.forEach((doc) => {
+      data.push(doc.id);
+    });
+    // readResult.map
+    functions.logger.log(readResult);
+  }
+  res.json({
+    result: data,
+  });
+});
