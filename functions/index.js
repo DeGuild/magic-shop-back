ae9ad741e18d7938a9a552bb323a3677fad0d838
parmcoder <possawat2017@gmail.com>
2021-10-14 00:44:11 +0700

    Refractored code for CORS and trying to deploy using express

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,27 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+"use strict";
+
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+const express = require("express");
+//  const cookieParser = require('cookie-parser')();
+const cors = require("cors")({ origin: true });
+const app = express();
 
 // The Firebase Admin SDK to access Firestore.
 const admin = require("firebase-admin");
@@ -8,7 +30,7 @@ admin.initializeApp();
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.addMagicScroll = functions.https.onRequest(async (req, res) => {
+const addMagicScroll = async (req, res) => {
   // Grab the text parameter.
   const address = req.body.address;
   const tokenId = req.body.tokenId;
@@ -33,9 +55,9 @@ exports.addMagicScroll = functions.https.onRequest(async (req, res) => {
   res.json({
     result: "Successful",
   });
-});
+};
 
-exports.readMagicScroll = functions.https.onRequest(async (req, res) => {
+const readMagicScroll = async (req, res) => {
   // Grab the text parameter.
   const paths = req.path.split("/");
   const address = paths[1];
@@ -58,9 +80,9 @@ exports.readMagicScroll = functions.https.onRequest(async (req, res) => {
       message: "Magic scroll not found!",
     });
   }
-});
+};
 
-exports.deleteMagicScroll = functions.https.onRequest(async (req, res) => {
+const deleteMagicScroll = async (req, res) => {
   // Grab the text parameter.
   const address = req.body.address;
   const tokenId = req.body.tokenId;
@@ -75,9 +97,9 @@ exports.deleteMagicScroll = functions.https.onRequest(async (req, res) => {
   res.json({
     result: "Successful",
   });
-});
+};
 
-exports.deleteMagicShop = functions.https.onRequest(async (req, res) => {
+const deleteMagicShop = async (req, res) => {
   // Grab the text parameter.
   const address = req.body.address;
   // Push the new message into Firestore using the Firebase Admin SDK.
@@ -87,9 +109,9 @@ exports.deleteMagicShop = functions.https.onRequest(async (req, res) => {
   res.json({
     result: "Successful",
   });
-});
+};
 
-exports.allMagicShops = functions.https.onRequest(async (req, res) => {
+const allMagicShops = async (req, res) => {
   // Grab the text parameter.
   const lastItem = req.path;
   const paths = req.path.split("/");
@@ -140,4 +162,14 @@ exports.allMagicShops = functions.https.onRequest(async (req, res) => {
   res.json({
     result: data.sort(),
   });
-});
+};
+
+app.use(cors);
+app.post("/addMagicScroll", addMagicScroll);
+app.get("/readMagicScroll/:address", readMagicScroll);
+app.post("/deleteMagicShop", deleteMagicShop);
+app.post("/deleteMagicScroll", deleteMagicScroll);
+app.get("/allMagicShops/:address/:direction", allMagicShops);
+app.get("/allMagicShopsOnce", allMagicShops);
+
+exports.app = functions.https.onRequest(app);
